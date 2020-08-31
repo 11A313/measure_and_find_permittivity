59f1efd45ab56d2c77a7ce96678b45bb73645485
11A313 <36575620+11A313@users.noreply.github.com>
2020-08-31 15:26:23 +0300

    Create measure_and_find_permittivity.m

---
+++ measure_and_find_permittivity.m
@@ -0,0 +1,56 @@
+
+catalogue{2,1} = xlsread('honeycombEpsilon_wallPermittivity2_wallThickness1mm.xlsx');
+catalogue{2,2} = xlsread('honeycombEpsilon_wallPermittivity2_wallThickness1.5mm.xlsx');
+catalogue{2,3} = xlsread('honeycombEpsilon_wallPermittivity2_wallThickness2mm.xlsx');
+catalogue{2,4} = xlsread('honeycombEpsilon_wallPermittivity2_wallThickness3mm.xlsx');
+catalogue{3,1} = xlsread('honeycombEpsilon_wallPermittivity3_wallThickness1mm.xlsx');
+catalogue{3,2} = xlsread('honeycombEpsilon_wallPermittivity3_wallThickness1.5mm.xlsx');
+catalogue{3,3} = xlsread('honeycombEpsilon_wallPermittivity3_wallThickness2mm.xlsx');
+catalogue{3,4} = xlsread('honeycombEpsilon_wallPermittivity3_wallThickness3mm.xlsx');
+catalogue{4,1} = xlsread('honeycombEpsilon_wallPermittivity4_wallThickness1mm.xlsx');
+catalogue{4,2} = xlsread('honeycombEpsilon_wallPermittivity4_wallThickness1.5mm.xlsx');
+catalogue{4,3} = xlsread('honeycombEpsilon_wallPermittivity4_wallThickness2mm.xlsx');
+catalogue{4,4} = xlsread('honeycombEpsilon_wallPermittivity4_wallThickness3mm.xlsx');
+catalogue{5,1} = xlsread('honeycombEpsilon_wallPermittivity5_wallThickness1mm.xlsx');
+catalogue{5,2} = xlsread('honeycombEpsilon_wallPermittivity5_wallThickness1.5mm.xlsx');
+catalogue{5,3} = xlsread('honeycombEpsilon_wallPermittivity5_wallThickness2mm.xlsx');
+catalogue{5,4} = xlsread('honeycombEpsilon_wallPermittivity5_wallThickness3mm.xlsx');
+catalogue{6,1} = xlsread('honeycombEpsilon_wallPermittivity6_wallThickness1mm.xlsx');
+catalogue{6,2} = xlsread('honeycombEpsilon_wallPermittivity6_wallThickness1.5mm.xlsx');
+catalogue{6,3} = xlsread('honeycombEpsilon_wallPermittivity6_wallThickness2mm.xlsx');
+catalogue{6,4} = xlsread('honeycombEpsilon_wallPermittivity6_wallThickness3mm.xlsx');
+catalogue{7,1} = xlsread('honeycombEpsilon_wallPermittivity7_wallThickness1mm.xlsx');
+catalogue{7,2} = xlsread('honeycombEpsilon_wallPermittivity7_wallThickness1.5mm.xlsx');
+catalogue{7,3} = xlsread('honeycombEpsilon_wallPermittivity7_wallThickness2mm.xlsx');
+catalogue{7,4} = xlsread('honeycombEpsilon_wallPermittivity7_wallThickness3mm.xlsx');
+catalogue{8,1} = xlsread('honeycombEpsilon_wallPermittivity8_wallThickness1mm.xlsx');
+catalogue{8,2} = xlsread('honeycombEpsilon_wallPermittivity8_wallThickness1.5mm.xlsx');
+catalogue{8,3} = xlsread('honeycombEpsilon_wallPermittivity8_wallThickness2mm.xlsx');
+catalogue{8,4} = xlsread('honeycombEpsilon_wallPermittivity8_wallThickness3mm.xlsx');
+catalogue{9,1} = xlsread('honeycombEpsilon_wallPermittivity9_wallThickness1mm.xlsx');
+catalogue{9,2} = xlsread('honeycombEpsilon_wallPermittivity9_wallThickness1.5mm.xlsx');
+catalogue{9,3} = xlsread('honeycombEpsilon_wallPermittivity9_wallThickness2mm.xlsx');
+catalogue{9,4} = xlsread('honeycombEpsilon_wallPermittivity9_wallThickness3mm.xlsx');
+
+
+
+liquidPermittivity = 50;
+materialPermittivity = 3;
+sideLength = 2.5;
+wallThickness = 3;
+
+
+for j = 1:4
+    if isequal( catalogue{materialPermittivity,j}(1,1) , wallThickness )
+        catalogueWallThickness = catalogue{materialPermittivity,j}(1,1);
+        catalogue{materialPermittivity,j}(1,1) = 0;     
+        sideLengthIndex = catalogue{materialPermittivity,j}(1,:) == ones(1 ,length(catalogue{materialPermittivity,j}(1,:)) ) *sideLength;
+        sideLengthIndex = find(sideLengthIndex==1);
+        liquidIndex = catalogue{materialPermittivity,j}(:,1) == ones( length(catalogue{materialPermittivity,j}(:,1)) , 1) * liquidPermittivity;
+        liquidIndex = find(liquidIndex==1);  
+        catalogue{materialPermittivity,j}(1,1) = catalogueWallThickness;      
+        catalogue{materialPermittivity,j}(liquidIndex,sideLengthIndex)
+    end
+end
+
+
